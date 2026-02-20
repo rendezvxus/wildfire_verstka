40544568d427ddca279fa352dc53c29f34319dbf
rendezvxus <realsuppaplex@gmail.com>
2026-02-20 22:45:59 +0300

    Navigation arrows finished

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ new Swiper('.slider', {
     slidesPerView: 3,
     spaceBetween: 60,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.swiper-button-next-custom',
+        prevEl: '.swiper-button-prev-custom'
     }
 });
